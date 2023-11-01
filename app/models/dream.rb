6bec0518ca93e53634cd9fbfa2d48f0843e0bcce
Leo <leocsdev@gmail.com>
2023-11-01 12:03:08 +0800

    rails db:migrate

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: dreams
+#
+#  id         :bigint           not null, primary key
+#  body       :text             not null
+#  slug       :string
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Dream < ApplicationRecord
 end
