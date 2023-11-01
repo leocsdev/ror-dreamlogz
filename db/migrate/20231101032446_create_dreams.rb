3f8ade9c98f485b34b91f5f8399f38be3de636b9
Leo Santillan <38428303+leocsdev@users.noreply.github.com>
2023-11-01 11:52:42 +0800

    Create dream resource (#4)

---
+++ db/migrate/20231101032446_create_dreams.rb
@@ -0,0 +1,11 @@
+class CreateDreams < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dreams do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
