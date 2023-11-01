b60942fa7f170f98dbfa67ebf407b026968ec600
Leo <leocsdev@gmail.com>
2023-11-01 11:45:08 +0800

    Add db constraint to title and body.

--- db/migrate/20231101032446_create_dreams.rb
+++ db/migrate/20231101032446_create_dreams.rb
@@ -1,8 +1,8 @@
 class CreateDreams < ActiveRecord::Migration[7.0]
   def change
     create_table :dreams do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :body, null: false
       t.string :slug
 
       t.timestamps
