97cc1fb14a9849ba2a69003f7238b89c2345b49a
Leo Santillan <38428303+leocsdev@users.noreply.github.com>
2023-11-01 14:05:39 +0800

    Create log dream page (#8)

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,3 +1,25 @@
 class DreamsController < ApplicationController
-  def index; end
+  def index
+    
+  end
+
+  def new
+    @dream = Dream.new
+  end
+
+  def create
+    @dream = Dream.new(dream_params)
+
+    if @dream.save
+      redirect_to dreams_path, success: "Your dream has been saved."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def dream_params
+    params.require(:dream).permit(:title, :body)
+  end
 end
--- app/models/dream.rb
+++ app/models/dream.rb
@@ -10,4 +10,5 @@
 #  updated_at :datetime         not null
 #
 class Dream < ApplicationRecord
+  validates :title, :body, presence: true
 end
