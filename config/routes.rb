8c366312a170ed2460aae684fc184c7190b785f9
Leo <leocsdev@gmail.com>
2023-11-01 11:34:57 +0800

    rails g resource Dream title body:text slug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :dreams
   root "static_pages#home"
 
   get "about", to: "static_pages#about"
---
+++ db/migrate/20231101032446_create_dreams.rb
@@ -0,0 +1,11 @@
+class CreateDreams < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dreams do |t|
+      t.string :title
+      t.text :body
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
