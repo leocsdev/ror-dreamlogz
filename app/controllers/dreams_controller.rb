821d043cf53a92bac7a87329d8763290e0fc4c54
Leo Santillan <38428303+leocsdev@users.noreply.github.com>
2023-11-02 17:31:45 +0800

    Add dream update and delete (#11)

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -17,6 +17,31 @@ class DreamsController < ApplicationController
     end
   end
 
+  def show
+    @dream = Dream.find(params[:id])
+  end
+  
+  def edit
+    @dream = Dream.find(params[:id])
+  end
+
+  def update
+    @dream = Dream.find(params[:id])
+
+    if @dream.update(dream_params)
+      redirect_to @dream, success: "Your dream has been updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @dream = Dream.find(params[:id])
+    @dream.destroy
+
+    redirect_to dreams_path, success: "Your dream has been deleted."
+  end
+
   private
 
   def dream_params
