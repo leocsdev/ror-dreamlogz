5b54cfd7e9c36ad453e3a4cec6f81b49a6414cdb
Leo <leocsdev@gmail.com>
2024-02-25 20:34:07 +0800

    Modify migration file for adding dream_date

--- db/migrate/20240225055610_add_dream_date_to_dreams.rb
+++ db/migrate/20240225055610_add_dream_date_to_dreams.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class AddDreamDateToDreams < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :dreams, :dream_date, :datetime, default: -> { "CURRENT_TIMESTAMP" }, null: false
+  end
+
+  def down
+    remove_column :dreams, :dream_date
   end
 end
